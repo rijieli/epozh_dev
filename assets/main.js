c738ebd42dde99d651ebee6c58c5ca3022eb1fa8
rijieli <rijie.li@outlook.com>
2020-10-30 22:34:15 +0800

    实现搜索页面与全局搜索

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,26 @@
-// 数据结构
-// JSON - 自定义分类 - 自定义数据 URL
-// 分类
-// - 标题
-// - URL
-
-// 获取索引 JSON
-// 匹配关键字并返回结果
 // 设置无匹配时返回数据
 // 按分类展示关键字
-// TODO: 阻止 submit 提交
-// TODO：默认渲染最新几篇文章
+
 (function () {
     "use strict";
-    let searchData = { "post": [] };
+    let siteMapData = { "post": [] };
     let searchAPIStatus = 200;
     let searchInputField = document.querySelector("#search-input-field");
-    let searchListDomNode = document.querySelector("#search-list");
+    let searchIcon = document.querySelector("#search-icon");
+    let searchResultDomNode = document.querySelector("#search-result");
+
+    let globalSearchInputField = document.querySelector("#global-search-input-field");
+    let globalSearchClearButton = document.querySelector("#global-search-clear");
+    let globalSearchResultDomNode = document.querySelector("#global-search-result");
+
+    let categoryMap = { "post": "博客", "work": "工作", "code": "CODE", "category": "分类" };
+
+    searchIcon.addEventListener("click", toggleSearchInput);
+
+    function toggleSearchInput() {
+        searchInputField.classList.toggle("hidden");
+        searchInputField.focus();
+    }
 
     function debounce(func, wait) {
         let timeout;
@@ -28,7 +33,8 @@
         }
     }
 
-    function requestListener() {
+    // 获取初始文档
+    function fetchSitemap() {
         if (this.status != 200) {
             searchAPIStatus = 500;
             return;
@@ -36,23 +42,65 @@
 
         let content = document.createElement("p");
         content.innerText = this.responseText;
-        // document.querySelector("#content").appendChild(content);
 
-        searchData = JSON.parse(this.responseText);
+        siteMapData = JSON.parse(this.responseText);
     }
 
-    function processData(keyword) {
+    function processData(keyword, resultNode, resultLimit) {
 
-        let result = {"post": []};
+        let result = {};
 
-        for(let item of searchData["post"]) {
-            if(item["title"].search(new RegExp(keyword, "i")) != -1) {
-                result["post"].push(item)
+        for (let category in siteMapData) {
+            result[category] = [];
+            if (siteMapData[category].length > 0) {
+                siteMapData[category].forEach(item => {
+                    if (item["title"].search(new RegExp(keyword, "i")) != -1) {
+                        result[category].push(item);
+                    }
+                })
             }
         }
 
-        console.log(result);
-        searchListDomNode.appendChild(generateDom(result["post"][0]["title"], result["post"][0]["url"]));
+        let searchResult = [];
+
+        for (let category in result) {
+            if (result[category].length > 0) {
+                searchResult.push(assembleResult(keyword, category, result[category], resultLimit));
+            }
+        }
+
+        searchResult.forEach(domNode => {
+            resultNode.appendChild(domNode);
+        })
+    }
+
+    function assembleResult(keyword, category, matchedItems, resultLimit) {
+        let resultGroupDivNode = document.createElement("div");
+        resultGroupDivNode.classList.add("result-group");
+        let titleNode = document.createElement("div");
+        titleNode.classList.add("search-result-title");
+        let titleTextNode = document.createElement("h3");
+        titleTextNode.innerText = categoryMap[category.toLowerCase()];
+
+        titleNode.appendChild(titleTextNode);
+        resultGroupDivNode.appendChild(titleNode);
+        let ulNode = document.createElement("ul");
+
+        if (resultLimit != -1 && matchedItems.length > resultLimit) {
+            matchedItems = matchedItems.slice(0, resultLimit);
+            let moreResultANode = document.createElement("a");
+            moreResultANode.href = "/search?q=" + escape(keyword);
+            moreResultANode.innerText = "MORE";
+            titleNode.appendChild(moreResultANode);
+        }
+
+        matchedItems.forEach(item => {
+            ulNode.appendChild(generateDom(item["title"], item["url"]));
+        })
+
+        resultGroupDivNode.appendChild(ulNode);
+
+        return resultGroupDivNode;
     }
 
     function generateDom(title, url) {
@@ -65,20 +113,36 @@
         return liDomNode;
     }
 
-    function handleInput() {
+    function searchInput() {
+        searchResultDomNode.innerHTML = "";
+        searchResultDomNode.classList.remove("hidden");
 
         let KEYWORD = searchInputField.value.trim();
-
-        processData(KEYWORD);
+        if (KEYWORD.length > 0) processData(KEYWORD, searchResultDomNode, 3);
     }
 
-    let inputFunc = debounce(handleInput, 500);
+    function globalSearchInput() {
+        globalSearchResultDomNode.innerHTML = "";
+        let KEYWORD = globalSearchInputField.value.trim();
+        if (KEYWORD.length > 0) processData(KEYWORD, globalSearchResultDomNode, -1);
+    }
 
-    searchInputField.addEventListener("input", inputFunc);
+    let searchInputHander = debounce(searchInput, 300);
+    let globalSearchInputHander = debounce(globalSearchInput, 300);
+
+    searchInputField.addEventListener("input", searchInputHander);
+    if (globalSearchInputField) globalSearchInputField.addEventListener("input", globalSearchInputHander);
+    if (globalSearchClearButton) globalSearchClearButton.addEventListener("click", e => {
+        globalSearchInputField.value = "";
+    })
+
+    document.body.addEventListener("click", () => {
+        searchResultDomNode.classList.add("hidden");
+    })
 
     var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", requestListener);
-    oReq.open("GET", "/search.json");
+    oReq.addEventListener("load", fetchSitemap);
+    oReq.open("GET", "/sitemap.json");
     oReq.send();
 })()
 
